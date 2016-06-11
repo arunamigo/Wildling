0a7aa04b310fe7009c1e1bcc294e6e9e1ff0b7db
arun gajaraj <arunamigo@live.com>
2016-06-11 15:09:30 +0530

    String Class Modified!

--- StrinChap.java
+++ StrinChap.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class StrinChap{
 	
 
@@ -13,5 +14,34 @@ public class StrinChap{
 	for (int a:ids)
 		System.out.println(a);
 	System.out.println(types);
+
+	System.out.println(Arrays.toString(ids)); //Converts Array into string and prints
+
+	String[] mammals = {"Monkey", "Chimp", "Donkey"};
+	System.out.println(Arrays.toString(mammals));
+	System.out.println("Length of mammals is " + mammals.length);
+	Arrays.sort(mammals);
+	System.out.println("After Sort");
+	System.out.println(Arrays.toString(mammals));
+
+	String multiDimArray [][] = new String [3][2];
+	for (int row=0;row<multiDimArray.length ; row++) {
+		for (int col=0;col<multiDimArray[row].length;col++) {
+			System.out.println(multiDimArray[row][col]);
+		}System.out.println();
+	}
+
+	//Another way to create Multi Dim Assymetric array
+
+	int [] [] multiDim2 = new int[3] [];
+	multiDim2[0] = new int[3];
+	multiDim2[1] = new int[4];
+	multiDim2[2] = new int[5];
+	for (int row=0;row<multiDim2.length ; row++) {
+		for (int col=0;col<multiDim2[row].length;col++) {
+			multiDim2[row][col]=row+col;
+			System.out.print(multiDim2[row][col]+ "\t" );
+		}System.out.println();
+	}
 	}
 }
